70b930e6c006daa75655254371975c17b7d60db0
Billy Davis <billah@gmail.com>
2015-06-09 19:44:08 -0500

    Check if composer vendor bin exists

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
-set --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
+if test -e ~/.composer/vendor/bin
+	set --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
+end
+
 
 . ~/.config/fish/fish_prompt2.fish
 
