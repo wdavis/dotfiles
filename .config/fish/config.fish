84e9886e090f736e5db829172d2f8fda68101df7
Billy Davis <bdavis@liquidfish.com>
2016-11-01 12:23:21 -0500

    update path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
-if test -e ~/.composer/vendor/bin
-	set --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
-end
+#if test -e ~/.composer/vendor/bin
+#	set --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
+#end
 
 set fish_greeting
+
+set --export PATH /usr/local/lib/node_modules $PATH
 
