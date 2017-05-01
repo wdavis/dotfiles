20312991deff29e368f7efd20c1d106c5fa27f58
Billy Davis <billah@gmail.com>
2017-05-01 10:37:09 -0500

    Removing some paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,2 @@
-if test -e ~/.composer/vendor/bin
-	set --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
-end
-
-set --universal fish_user_paths $fish_user_paths ~/dotfiles/bin
-
 set fish_greeting
 
